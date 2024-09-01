8c522af388cfbba55bbc4c0e4b0dfea38faff55b
sushistack <kjm71712@naver.com>
2024-09-01 12:38:50 +0900

    페이징 기능 추가

--- src/main/kotlin/com/sushistack/datajpa/controller/MemberController.kt
+++ src/main/kotlin/com/sushistack/datajpa/controller/MemberController.kt
@@ -1,8 +1,12 @@
 package com.sushistack.datajpa.controller
 
+import com.sushistack.datajpa.dto.MemberDTO
 import com.sushistack.datajpa.entity.Member
 import com.sushistack.datajpa.repository.MemberRepository
 import jakarta.annotation.PostConstruct
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -12,7 +16,10 @@ class MemberController(private val memberRepository: MemberRepository) {
 
     @PostConstruct
     fun init() {
-        Member(username = "memberA", age = 10).let { memberRepository.save(it) }
+        (1..100).forEach {
+            Member(username = "member${it}", age = it).let { m -> memberRepository.save(m) }
+        }
+
     }
 
     @GetMapping("/members/{id}")
@@ -20,4 +27,10 @@ class MemberController(private val memberRepository: MemberRepository) {
 
     @GetMapping("/members2/{id}")
     fun findMember2(@PathVariable("id") member: Member) = member.username
+
+    // ?page=0&size=10&sort=username,desc&sort=id,asc
+    @GetMapping("/members")
+    fun findMembers(@PageableDefault(size = 5, sort = ["username"]) pageable: Pageable): Page<MemberDTO> =
+        memberRepository.findAll(pageable)
+            .map { MemberDTO(it.id, it.username, it.team?.name ?: "") }
 }
